39a277a157a710aff30e353ea7033803a18d1f4e
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2019-05-10 08:42:52 -0400

    Include namespace variable as optional for deployment

--- deployment/service.tf
+++ deployment/service.tf
@@ -1,19 +1,20 @@
 resource "kubernetes_service" "example" {
-    metadata {
-        name = var.app.name
-        labels = {
-            app = var.app.name
-        }
+  metadata {
+    name = var.app.name
+    namespace = var.namespace
+    labels = {
+      app = var.app.name
+      }
+  }
+
+  spec {
+    selector = {
+      app = var.app.name
     }
 
-    spec {
-        selector = {
-            app = var.app.name
-        }
-
-        port {
-            port = var.service_port
-            target_port = var.app.port
-        }
+    port {
+      port = var.service_port
+      target_port = var.app.port
     }
+  }
 }
