05624d218d1401e81ecf71b0c707da1100d6c645
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2019-08-26 14:34:36 -0400

    Add variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,21 +1,26 @@
 variable "namespace" {
   default = null
+  description = "A kubernetes namespace to deploy to, default is default"
 }
 
 variable "name" {
   type = string
+  description = "A name for the kubernetes deployment"
 }
 
 variable "user_id" {
   default = 1001
+  description = "A user id for unprivileged containers, default is 1001"
 }
 
 variable "replicas" {
   default = 1
+  description = "A number of replicas for the deployment, default is 1"
 }
 
 variable "service_port" {
   default = 80
+  description = "A service port for a Kubernetes service, default is 80"
 }
 
 variable "containers" {
@@ -24,4 +29,5 @@ variable "containers" {
     image = string,
     port  = number
   }))
+  description = "A list of container objects, including name, image, and port to create"
 }
